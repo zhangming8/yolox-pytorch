f6b97cd78b070da85fecfa975f456583b4a2ad2e
mayong <mayong@qianxin.com>
2021-08-04 22:03:52 +0800

    update files.

--- train.cmd
+++ train.cmd
@@ -3,7 +3,9 @@ rem  train from scratch
 rem python train.py gpus='0' backbone="CSPDarknet-s" num_epochs=300 exp_id="coco_CSPDarknet-s_640x640" use_amp=False val_intervals=1 data_num_workers=8
 
 rem load pre-train weight
-python train.py gpus='0' backbone="CSPDarknet-s" num_epochs=300 exp_id="coco_CSPDarknet-s_640x640" use_amp=False val_intervals=1 data_num_workers=4 load_model="f:/weights/yolox-s.pth" resume=False  dataset_path=D:/coco_dataset
+rem  python train.py gpus='0' backbone="CSPDarknet-s" num_epochs=300 exp_id="coco_CSPDarknet-s_640x640" use_amp=False val_intervals=1 data_num_workers=4 load_model="f:/weights/yolox-s.pth" resume=False  dataset_path=D:/coco_dataset
+
+python train.py gpus='0' backbone="CSPDarknet-nano" num_epochs=300 exp_id="coco_CSPDarknet-nano_640x640" use_amp=False val_intervals=1 data_num_workers=4 load_model="f:/weights/yolox-nano.pth" resume=False  dataset_path=D:/coco_dataset
 
 rem  resume 'model_best.pth / model_num.pth', includes weight and epoch
 rem python train.py gpus='0' backbone="CSPDarknet-s" num_epochs=300 exp_id="coco_CSPDarknet-s_640x640" use_amp=False val_intervals=1 data_num_workers=8 load_model="exp/coco_CSPDarknet-s_640x640/model_best.pth" resume=True
