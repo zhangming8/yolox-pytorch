b9bdfab8f8e43aa624292b110ace155a5add516e
mayong <mayong@qianxin.com>
2021-08-04 22:03:39 +0800

    update files.

--- deployment/onnx/convert/pth2onnx.sh
+++ deployment/onnx/convert/pth2onnx.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
+rem  converting the best model to an onnx model
+python pth2onnx.py  backbone="CSPDarknet-s" load_model="../../../exp/coco_CSPDarknet-s_640x640/model_best.pth" 
+python pth2onnx.py  backbone="CSPDarknet-nano" load_model="../../../exp/coco_CSPDarknet-nano_640x640/model_best.pth" 
 
-#  reproduce the results on COCO val2017 and test2017:
-#  evaluate COCO val2017
-# python evaluate.py gpus='0' backbone="CSPDarknet-nano" load_model="../weights/yolox-nano.pth" dataset_path="/data/dataset/coco_dataset"
-# python evaluate.py gpus='0' backbone="CSPDarknet-tiny" load_model="../weights/yolox-tiny.pth" dataset_path="/data/dataset/coco_dataset"
-# python evaluate.py gpus='0' backbone="CSPDarknet-s" load_model="../weights/yolox-s.pth" dataset_path="/data/dataset/coco_dataset"
-# python evaluate.py gpus='0' backbone="CSPDarknet-m" load_model="../weights/yolox-m.pth" dataset_path="/data/dataset/coco_dataset"
-# python evaluate.py gpus='0' backbone="CSPDarknet-l" load_model="../weights/yolox-l.pth" dataset_path="/data/dataset/coco_dataset"
-# python evaluate.py gpus='0' backbone="CSPDarknet-x" load_model="../weights/yolox-x.pth" dataset_path="/data/dataset/coco_dataset"
-# python evaluate.py gpus='0' backbone="CSPDarknet-x" load_model="../weights/yolox-x.pth" dataset_path="/data/dataset/coco_dataset" test_size="(800,800)"
-#  evaluate COCO test2017, create upload file
-# python evaluate.py gpus='0' backbone="CSPDarknet-nano" load_model="../weights/yolox-nano.pth" dataset_path="/data/dataset/coco_dataset" test_ann="/data/dataset/coco_dataset/annotations/image_info_test-dev2017.json"
-# python evaluate.py gpus='0' backbone="CSPDarknet-tiny" load_model="../weights/yolox-tiny.pth" dataset_path="/data/dataset/coco_dataset" test_ann="/data/dataset/coco_dataset/annotations/image_info_test-dev2017.json"
-# python evaluate.py gpus='0' backbone="CSPDarknet-s" load_model="../weights/yolox-s.pth" dataset_path="/data/dataset/coco_dataset" test_ann="/data/dataset/coco_dataset/annotations/image_info_test-dev2017.json"
-# python evaluate.py gpus='0' backbone="CSPDarknet-m" load_model="../weights/yolox-m.pth" dataset_path="/data/dataset/coco_dataset" test_ann="/data/dataset/coco_dataset/annotations/image_info_test-dev2017.json"
-# python evaluate.py gpus='0' backbone="CSPDarknet-l" load_model="../weights/yolox-l.pth" dataset_path="/data/dataset/coco_dataset" test_ann="/data/dataset/coco_dataset/annotations/image_info_test-dev2017.json"
-# python evaluate.py gpus='0' backbone="CSPDarknet-x" load_model="../weights/yolox-x.pth" dataset_path="/data/dataset/coco_dataset" test_ann="/data/dataset/coco_dataset/annotations/image_info_test-dev2017.json"
-# python evaluate.py gpus='0' backbone="CSPDarknet-x" load_model="../weights/yolox-x.pth" dataset_path="/data/dataset/coco_dataset" test_ann="/data/dataset/coco_dataset/annotations/image_info_test-dev2017.json" test_size="(800,800)"
-
-#  converting the best model to an onnx model
-python pth2onnx.py  backbone="CSPDarknet-s" load_model="../../exp/coco_CSPDarknet-s_640x640/model_best.pth" 
